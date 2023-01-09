82f8772a9148037ada535bcb93fe122f9f7fb6dc
JAYABHARGAVI B <jayabhargavi2504@gmail.com>
2023-01-09 21:39:39 +0530

    Sample Commit

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -71,7 +71,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = 'https://stack-overflow-j.netlify.app'
 
     const handlePostAns = (e, answerLength) =>{
         e.preventDefault()
